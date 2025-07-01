d22ed7d527e1486e8ba11027c4e36f274cd69791
static-motion <valentin.mihov7@gmail.com>
2025-07-01 16:43:11 +0300

    Removed required keyword from navigation properties which were making it impossible to instantiate an object from the class.

--- PizzaApp.Data.Models/MappingEntities/PizzaTopping.cs
+++ PizzaApp.Data.Models/MappingEntities/PizzaTopping.cs
@@ -7,11 +7,11 @@
     {
         [Comment("Foreign Key to Pizzas, part of composite Primary Key.")]
         public int PizzaId { get; set; }
-        public required Pizza Pizza { get; set; }
+        public virtual Pizza Pizza { get; set; } = null!;
 
         [Comment("Foreign Key to Toppings, part of composite Primary Key.")]
         public int ToppingId { get; set; }
 
-        public required Topping Topping { get; set; }
+        public virtual Topping Topping { get; set; } = null!;
     }
 }
