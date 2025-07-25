9cc0f7ccefdd2be9065380d1601b2a0f87fa4a65
Valentin Mihov <valentin.mihov7@gmail.com>
2025-07-25 20:28:06 +0300

    Added account dropdown navigation.

--- PizzaApp.Services.Common/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PizzaApp.Services.Common
-{
-    public static class Constants
-    {
-        public const string AdminUserId = "7BC9CF3B-7464-4B4A-EA3B-08DDB8A10943";
-    }
-}
---
+++ PizzaApp.Web/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿namespace PizzaApp.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AccountController : BaseController
+    {
+        public async Task<IActionResult> Index()
+        {
+            return this.View();
+        }
+    }
+}
