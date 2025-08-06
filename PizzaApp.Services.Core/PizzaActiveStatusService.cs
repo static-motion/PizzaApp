f6462ddece4370e0d850268b5d4d33899d4f74b9
Valentin Mihov <valentin.mihov7@gmail.com>
2025-08-07 00:00:43 +0300

    fixed not implemented method.

--- PizzaApp.Services.Core/PizzaActiveStatusService.cs
+++ PizzaApp.Services.Core/PizzaActiveStatusService.cs
@@ -7,7 +7,11 @@
     {
         public bool IsPizzaActive(Pizza pizza)
         {
-            throw new NotImplementedException();
+            return pizza.IsDeleted == false // the pizza must be active
+                    && (pizza.Sauce == null || pizza.Sauce.IsDeleted == false) // the sauce must be either not set or active
+                    && pizza.Dough.IsDeleted == false // the dough must be active
+                    && pizza.Toppings.All(t => t.Topping.IsDeleted == false // all toppings must be active
+                    && t.Topping.ToppingCategory.IsDeleted == false); // all categories must be active
         }
     }
 }
